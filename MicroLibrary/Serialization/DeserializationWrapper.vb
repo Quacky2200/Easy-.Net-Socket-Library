807ee0bf154004df4244da5336e6a4f76d0329c9
Vincent <Pathogenix@users.noreply.github.com>
2015-04-09 05:24:05 -0500

    added method reflection support

--- MicroLibrary/Serialization/DeserializationWrapper.vb
+++ MicroLibrary/Serialization/DeserializationWrapper.vb
@@ -3,6 +3,7 @@
 Namespace Serialization
     <Serializable>
     Public Class DeserializationWrapper
+        Public Shared Property ReflectionFlags As BindingFlags = BindingFlags.Instance Or BindingFlags.[Public]
         Public Sub New()
         End Sub
 
@@ -31,20 +32,19 @@ Namespace Serialization
         End Sub
 
         Public Shared Function GetPropertyReferences(target As Object) As List(Of PropertyReference)
-            Dim TargetType As Type = target.GetType
-            Dim objProperties As PropertyInfo() = TargetType.GetProperties(BindingFlags.Instance Or BindingFlags.[Public])
-            Dim instance As Object = Activator.CreateInstance(TargetType)
-            Dim index As Integer = 0
-            Dim References As New List(Of PropertyReference)
-            For Each p As PropertyInfo In objProperties
-                References.Add(New PropertyReference(instance, p, index))
-                index += 1
-            Next
-            Return References
+            Return GetPropertyReferences(target.GetType, ReflectionFlags)
+        End Function
+
+        Public Shared Function GetPropertyReferences(target As Object, ReflectionFlags As BindingFlags) As List(Of PropertyReference)
+            Return GetPropertyReferences(target.GetType, ReflectionFlags)
         End Function
 
         Public Shared Function GetPropertyReferences(Type As Type) As List(Of PropertyReference)
-            Dim objProperties As PropertyInfo() = Type.GetProperties(BindingFlags.Instance Or BindingFlags.[Public])
+            Return GetPropertyReferences(Type, ReflectionFlags)
+        End Function
+
+        Public Shared Function GetPropertyReferences(Type As Type, ReflectionFlags As BindingFlags) As List(Of PropertyReference)
+            Dim objProperties As PropertyInfo() = Type.GetProperties(ReflectionFlags)
             Dim instance As Object = Activator.CreateInstance(Type)
             Dim index As Integer = 0
             Dim References As New List(Of PropertyReference)
@@ -54,6 +54,19 @@ Namespace Serialization
             Next
             Return References
         End Function
+
+        Public Shared Function GetMethodReferences(Type As Type, ReflectionFlags As BindingFlags) As List(Of MethodReference)
+            Dim objMethods As MethodInfo() = Type.GetMethods(ReflectionFlags)
+            Dim instance As Object = Activator.CreateInstance(Type)
+            Dim index As Integer = 0
+            Dim References As New List(Of MethodReference)
+            For Each m As MethodInfo In objMethods
+                References.Add(New MethodReference(instance, m, index))
+                index += 1
+            Next
+            Return References
+        End Function
+
     End Class
 
     Public Class PropertyReference
@@ -67,6 +80,20 @@ Namespace Serialization
         Public Property Instance As Object
         Public Property PropertyInfo As PropertyInfo
         Public Property PropertyIndex As Integer
+
+    End Class
+
+    Public Class MethodReference
+
+        Public Sub New(Instance As Object, MethodInfo As MethodInfo, MethodIndex As Integer)
+            Me.Instance = Instance
+            Me.MethodInfo = MethodInfo
+            Me.MethodIndex = MethodIndex
+        End Sub
+
+        Public Property Instance As Object
+        Public Property MethodInfo As MethodInfo
+        Public Property MethodIndex As Integer
 
     End Class
 End Namespace
